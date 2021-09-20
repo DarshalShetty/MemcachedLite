2e6761fbbc7f212efb235e4236d80d05b5f26c84
Darshal Shetty <darshalcs@gmail.com>
2021-09-20 15:58:22 -0400

    Add simple test

---
+++ tests/test_basic_requirements.py
@@ -0,0 +1,27 @@
+import os
+import sys
+import glob
+
+from src.client_sdk import ClientSDK
+from src.server_config import SERVER_HOST, SERVER_PORT, SERVER_STORAGE_DIR
+
+files = glob.glob('/YOUR/PATH/*')
+for f in files:
+    os.remove(f)
+
+if __name__ == "__main__":
+    with ClientSDK(SERVER_HOST, SERVER_PORT) as conn:
+        res = conn.get_str(["k1", "k2"])
+        assert res == {}
+
+        res = conn.set_str("k1", "abc")
+        assert res == "STORED"
+
+        res = conn.get_str(["k1", "k2"])
+        assert res == {"k1": "abc"}
+
+        res = conn.set_str("k2", "xyz")
+        assert res == "STORED"
+
+        res = conn.get_str(["k1", "k2"])
+        assert res == {"k1": "abc", "k2": "xyz"}
